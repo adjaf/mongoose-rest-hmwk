6b8694c97b2c4453296de022fc0d22b14dcf8321
Oscar Daniel Alcantara Lopez <oscar.alcantara@sciodev.com>
2022-04-29 22:49:19 -0500

    tarea oscar

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,6 +6,6 @@ router.get('/oscarEndpoint', productController.oscarEndpoint);
 router.get('/:id', productController.findProduct);
 router.post('/', productController.createProduct);
 router.patch('/:id', productController.updateProduct);
-router.delete('/', productController.deleteProduct);
+router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
