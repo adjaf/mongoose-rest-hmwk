c9c154b82e92349775e25a0f72b9a5dcdc66c15e
Fernando Cardoso <101157811+fernandoreki@users.noreply.github.com>
2023-03-29 17:53:58 -0600

    almost

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
     port: process.env.PORT || 3000,
-    db_host: process.env.DB_HOST || 'localhost',
+    db_host: process.env.DB_HOST || '127.0.0.1',
     db_port: process.env.DB_PORT || 27017,
     db_name: process.env.DB_NAME || 'stepone',
     secret: process.env.SECRET || 'secret'
