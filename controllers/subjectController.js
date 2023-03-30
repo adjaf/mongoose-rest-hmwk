8b6508b3407acdf62bb6a4e49762ace3b85c983e
Fernando Cardoso <feeerskateagt@gmail.com>
2023-03-30 01:01:25 -0600

    review code

--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -3,35 +3,136 @@ const SubjectModel = mongoose.model('Subject');
 
 exports.getAll = function(req, res) {
     // TODO: get all subjects
+    SubjectModel.find()
+        .then((subjects) => {
+            return res.send(subjects);
+        })
+        .catch(err => {
+            return res.status(400).send(err);
+        })
 }
 
-exports.getOne = function(req, res) {
+exports.getOne = async function(req, res) {
     // TODO: get one subject with their teachers and students
+    const id = req.params.id;
+    try {
+        const subject = await SubjectModel.findById(id);
+        if (!subject) {
+            return res.status(404).send({ err: "Subject not found" });
+        }
+        return res.status(200).json({
+            subject: subject.name,
+            data: {
+                teachers: subject.teacher,
+                students: subject.students
+            }
+        })
+    } catch (error) {
+        return res.status(400).send({ error });
+    }
 }
 
 exports.createSubject = function(req, res) {
     // TODO: Create subject
+    const body = req.body;
+    SubjectModel.create(body, function(err, subject) {
+        if (!subject) {
+            return res.status(422).send({ "error": true });
+        }
+        if (err) {
+            return res.status(400).send({ "error": true });
+        }
+        return res.send(subject);
+    });
 }
 
 exports.updateSubject = function(req, res) {
     // TODO: update subject
+    const id = req.params.id;
+    const body = req.body;
+
+    SubjectModel.findByIdAndUpdate(id, body,{ new: true }, function(err, subject) {
+        if (!subject) {
+            return res.status(404).send({ err: "Subject not found"});
+        }
+        if (err) {
+            return res.send({ err });
+        }
+        return res.send(subject);
+    })
 }
 
 exports.deleteSubject = function(req, res) {
     // TODO: Delete subject, if it has students, it can't be deleted
-}
-
-exports.getByName = function(req, res) {
-    // TODO: search subject by name
-}
-
-exports.assignTeacher = function(req, res) {
-    const teacherId = req.body.teacherId;
     const id = req.params.id;
 
+    SubjectModel.findByIdAndDelete(id, function(err, subject) {
+        if (err) {
+            return res.send({ err });
+        }
+        return res.status(204).send().json({message: 'Subject deleted successfully'});
+    })
+}
+
+exports.getByName = async function(req, res) {
+    // TODO: search subject by name
+    const search = req.params.search;
+    if (!search) {
+        let subjects = await SubjectModel.find();
+        return res.send(subjects);
+    }
+
+    try {
+        const subjects = await SubjectModel.find({
+             name: { '$regex': search, '$options': 'i' } 
+        });
+        
+        if (!subjects) {
+            return res.status(404).send({ err: "Subject not found" });
+        }
+
+        return res.send(subjects);
+    } catch (error) {
+        return res.status(400).send({ error });
+    }
+}
+
+exports.assignTeacher = async function(req, res) {
+    const teacherId = req.body.teacherId;
+    const id = req.params.id;
     // TODO: assign teacher to the subject, if it has a teacher, replace it
+    try {
+        const subject = await SubjectModel.findById(id);
+        
+        if (!subject) {
+            return res.status(404).send({ err: "Subject not found" });
+        }
+
+        if (!teacherId) {
+            return res.status(422).send({ err: "Please send a teacher" });
+        }
+
+        const teacher = await SubjectModel.findById(teacherId);
+
+        subject.teacher.push(teacherId);
+        teacher.subject.push(id);
+
+        await teacher.save();
+        const updatedSubject = await subject.save();
+
+        return res.send(updatedSubject);
+    } catch (error) {
+        return res.status(400).send({ error });
+    }
 }
 
 exports.withoutStudents = function(req, res) {
     // TODO: devolver materias sin students
+    SubjectModel.find({students:[]})
+        .then((subjects) => {
+            return res.send(subjects);
+        })
+        .catch(err => {
+            return res.status(400).send(err);
+        })
 }
--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -2,37 +2,103 @@ const mongoose = require('mongoose');
 const TeacherModel = mongoose.model('Teacher');
 const SubjectModel = mongoose.model('Subject');
 
-exports.getAll = function(req, res) {
+exports.getAll = function (req, res) {
     // TODO: Get all teachers
+    TeacherModel.find()
+        .then((subjects) => {
+            return res.send(subjects);
+        })
+        .catch(err => {
+            return res.status(400).send(err);
+        })
 }
 
-exports.getOne = async function(req, res) {
+exports.getOne = async function (req, res) {
     const id = req.params.id;
-    
     // TODO: Get one teacher with its subjects (I should display subject's name)
+    try {
+        const teacher = await TeacherModel.findById(id);
+        if (!teacher) {
+            return res.status(404).send({ err: "Teacher not found" });
+        }
+        const subject = await SubjectModel.findById(teacher.subjects);
+        const data = {
+            id: id,
+            first_name: teacher.first_name,
+            last_name: teacher.last_name,
+            subject: subject.name
+        }
+        return res.status(200).send(data);
+    } catch (error) {
+        return res.status(400).send({ error });
+    }
 }
 
-exports.createTeacher = function(req, res) {
+exports.createTeacher = function (req, res) {
     const body = req.body;
-
     // TODO: create teacher
+    TeacherModel.create(body, function(err, teacher) {
+        if (!teacher) {
+            return res.status(422).send({ "error": true });
+        }
+        if (err) {
+            return res.status(400).send({ "error": true });
+        }
+        return res.send(teacher);
+    });
 }
 
-exports.updateTeacher = function(req, res) {
+exports.updateTeacher = function (req, res) {
     const id = req.params.id;
     const body = req.body;
-
     // TODO: Update teacher
+    TeacherModel.findByIdAndUpdate(id, body,{ new: true }, function(err, teacher) {
+        if (!teacher) {
+            return res.status(404).send({ err: "Teacher not found"});
+        }
+        if (err) {
+            return res.send({ err });
+        }
+        return res.send(teacher);
+    })
 }
 
-exports.deleteTeacher = function(req, res) {
+exports.deleteTeacher = function (req, res) {
     const id = req.params.id;
     // TODO: Delete teacher, if it has subjects it shouldn't be deleted
+    TeacherModel.findByIdAndDelete(id, function(err, teacher) {
+        if (err) {
+            return res.send({ err });
+        }
+        return res.status(204).send().json({message: 'Teacher deleted successfully'});
+    })
 }
 
-exports.assignSubject = async function(req, res) {
+exports.assignSubject = async function (req, res) {
     const id = req.params.id;
     const subjectId = req.body.subjectId;
+    // TODO: Assign subject to teacher
+    try {
+        const teacher = await TeacherModel.findById(id);
+        
+        if (!teacher) {
+            return res.status(404).send({ err: "Teacher not found" });
+        }
 
-    // TODO: Assign subject to teacher 
+        if (!subjectId) {
+            return res.status(422).send({ err: "Please send a subject" });
+        }
+
+        const subject = await SubjectModel.findById(subjectId);
+
+        teacher.teacher.push(subjectId);
+        subject.teacher.push(id);
+
+        await subject.save();
+        const updatedTeacher = await teacher.save();
+
+        return res.send(updatedSubject);
+    } catch (error) {
+        return res.status(400).send({ error });
+    } 
 }
